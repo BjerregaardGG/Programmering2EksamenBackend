2b17dfbc29fdf7fccc96062dbef1101b0a217d61
BjerregaardGG <oliverbjerregaard1@gmail.com>
2025-06-12 10:39:55 +0200

    Controller, Repo, interface og service metoder + DTO

--- src/main/java/eksamen/programmering2eksamenbackend/Siren/SirenController.java
+++ src/main/java/eksamen/programmering2eksamenbackend/Siren/SirenController.java
@@ -1,7 +1,10 @@
 package eksamen.programmering2eksamenbackend.Siren;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import eksamen.programmering2eksamenbackend.Siren.SirenDTO;
 
 import java.util.List;
 
@@ -11,31 +14,65 @@ import java.util.List;
 @CrossOrigin("x")
 public class SirenController {
 
+    private final SirenServiceImpl sirenService;
+    private final SirenServiceImpl sirenServiceImpl;
+
+    @Autowired
+    public SirenController(SirenServiceImpl sirenService, SirenServiceImpl sirenServiceImpl){
+        this.sirenService = sirenService;
+        this.sirenServiceImpl = sirenServiceImpl;
+    }
+
     @GetMapping
     public ResponseEntity<List<SirenDTO>> getAllSirens(){
+        try {
+            List<SirenDTO> sirens = sirenService.findAllSirens();
+            return ResponseEntity.ok(sirens);
 
+        }catch(Exception e){
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<SirenDTO> getSirenById(@PathVariable Long id){
+    public ResponseEntity<SirenDTO> getSirenById(@PathVariable int id) {
+        SirenDTO siren;
+        try {
+            siren = sirenService.findSirenById(id);
+            return ResponseEntity.ok(siren);
 
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
     @PostMapping
-    public ResponseEntity<SirenDTO> createSiren(@RequestBody SirenDTO sirenDTO){
+    public ResponseEntity<SirenDTO> createSiren(@RequestBody SirenDTO siren){
+        try {
+            sirenService.createSiren(siren);
+            return ResponseEntity.ok(siren);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
 
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<SirenDTO> updateSiren(@PathVariable Long id, @RequestBody SirenDTO sirenDTO){
-
+    public ResponseEntity<SirenDTO> updateSiren (@PathVariable int id, @RequestBody SirenDTO sirenDTO){
+        return null;
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteSiren(@PathVariable Long id){
-
+    public ResponseEntity<Void> deleteSiren (@PathVariable int id) {
+        try {
+            sirenService.deleteSiren(id);
+            return ResponseEntity.noContent().build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
     }
-}
-
-
 }
--- src/main/java/eksamen/programmering2eksamenbackend/Siren/SirenService.java
+++ src/main/java/eksamen/programmering2eksamenbackend/Siren/SirenService.java
@@ -1,4 +1,37 @@
 package eksamen.programmering2eksamenbackend.Siren;
 
+import java.util.List;
+
 public interface SirenService {
+
+    // henter alle sirener
+    List<SirenDTO> findAllSirens();
+
+    // henter sirener ud fra id
+    SirenDTO findSirenById(int id);
+
+    // opretter en ny sirene
+    SirenDTO createSiren(SirenDTO sirenDTO);
+
+    // opdaterer en sirene
+    SirenDTO updateSiren(int id, SirenDTO sirenDTO);
+
+    // sletter en sirene
+    void deleteSiren(int id);
+
+    // finder en sirene inden for en given radius af et punkt
+    List<SirenDTO> findSirensWithinRadius(double latitude, double longitude, double radiusKm);
+
+    // aktiverer en liste af sirener
+    void activateSirens(List<SirenDTO> sirens);
+
+    // deaktiverer en liste af sirener
+    void deactivateSirens(List<SirenDTO> sirens);
+
+    // finder aktive sirener
+    List<SirenDTO> findActiveSirens();
+
+    // tjekker om en sirene kan aktiveres
+    boolean canActivateSiren(int id);
 }
+
